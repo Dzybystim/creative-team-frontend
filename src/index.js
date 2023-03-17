20deed71a27fa0f02d2ff1091eed48d3677c8a9c
Volkova Valeria <vvolk.valeria@gmail.com>
2023-03-17 23:42:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import  NoticesPage  from "./pages/NoticesPage";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <NoticesPage/>
+    <App />
   </React.StrictMode>
 );
