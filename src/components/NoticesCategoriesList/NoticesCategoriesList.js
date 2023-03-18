cad98ab9a7dc3e97fd5f0485d2158399b737cae2
Yevdokymenko <102970897+Yevdokymenko@users.noreply.github.com>
2023-03-18 23:58:34 +0200

    1

--- src/components/NoticesCategoriesList/NoticesCategoriesList.js
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.js
@@ -1,57 +1,55 @@
 //import {  useEffect } from 'react';
-import { NoticeCategoryItem } from "../NoticeCategoryItem/NoticeCategoryItem";
+import { NoticeCategoryItem } from '../NoticeCategoryItem/NoticeCategoryItem';
 //import { getNotices } from "../../API/fetchNotice";
 
 const items = [
-        {
-        id:1,
-        category: "sell",
-        title: "dog",
-        breed: "kolly",
-        location: "Lviv",
-        age: "5",
-        price: 75,
-        },
-        {
-        id:2,
-        category: "lost-found",
-        title: "dog",
-        breed: "main-kun",
-        location: "Kiev",
-        age: "2",
-        },
-        {
-        id:3,
-        category: "sell",
-        title: "dog",
-        breed: "corgi",
-        location: "Kharkiv",
-        age: "1",
-        price: 50,
-        }
-]
+  {
+    id: 1,
+    category: 'sell',
+    title: 'dog',
+    breed: 'kolly',
+    location: 'Lviv',
+    age: '5',
+    price: 75,
+  },
+  {
+    id: 2,
+    category: 'lost-found',
+    title: 'dog',
+    breed: 'main-kun',
+    location: 'Kiev',
+    age: '2',
+  },
+  {
+    id: 3,
+    category: 'sell',
+    title: 'dog',
+    breed: 'corgi',
+    location: 'Kharkiv',
+    age: '1',
+    price: 50,
+  },
+];
 
 const NoticesCategoriesList = ({ item }) => {
-   // const location = useLocation();
+  // const location = useLocation();
 
-const category = "sell";
+  // const category = "sell";
 
-    // useEffect(() => {
-    //     getNotices(category);
-    // }, [category]);
+  // useEffect(() => {
+  //     getNotices(category);
+  // }, [category]);
 
-
-    return (
-        <>
-        <button type="button">Add pet</button>
-<ul>
+  return (
+    <>
+      <button type="button">Add pet</button>
+      <ul>
         {items.map(item => {
-            return <NoticeCategoryItem key={item.id} item={item} />;
-          })}
-</ul>    
-        </> 
-    )
+          return <NoticeCategoryItem key={item.id} item={item} />;
+        })}
+      </ul>
+    </>
+  );
 };
-
 
 export default NoticesCategoriesList;
