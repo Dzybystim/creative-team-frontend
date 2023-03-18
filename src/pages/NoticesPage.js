467cfbc120fd99d39a3817733305c9c30c715a31
Volkova Valeria <vvolk.valeria@gmail.com>
2023-03-19 01:48:04 +0200

    NoticesPage

--- src/pages/NoticesPage.js
+++ src/pages/NoticesPage.js
@@ -3,9 +3,10 @@ import { NoticesCategoriesNav } from "../components/NoticesCategoriesNav/Notices
 import {NoticesSearch} from '../components/NoticesSearch/NoticesSearch'
 import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
+import css from "./NoticesPage.module.css";
 
 const NoticesPage = () => {
-     return (<div>
+     return (<div className={css.container}>
          <h2>Find your favorite pet</h2>
          <NoticesSearch/>
 
