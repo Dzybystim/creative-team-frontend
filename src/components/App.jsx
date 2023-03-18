2432177137d4de83aa5ba6a04f76f9a8f3c2e3d8
Dzybystim <103567184+Dzybystim@users.noreply.github.com>
2023-03-18 10:31:44 +0200

    Removed extra styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,6 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework template
     </div>
   );
