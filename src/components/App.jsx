9d1576af4264f705a84c965f3a4aebb670fab4fe
Oleksandr-Berd <alex.berd86@gmail.com>
2023-03-18 10:46:39 +0200

    ourfriends first step

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import OurFriendsPage from 'pages/OurFriendsPage';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <OurFriendsPage></OurFriendsPage>
     </div>
   );
 };
