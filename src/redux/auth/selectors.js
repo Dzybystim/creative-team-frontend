c8807b8f0d827a6097ba092a48dc1e1f509886f6
Olha Chornei <pradocw8053@gmail.com>
2023-03-19 19:09:17 +0200

    commit header

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,1 +1,6 @@
 export const getAuth = state => state.auth.auth;
+
+// export const getIsLoggedIn = state => state.auth.isLoggedIn;
+// export const getUserName = state => state.auth.user.name;
+// export const getIsFetchingCurrentUser = state =>
+//   state.auth.isFetchingCurrentUser;
--- src/redux/selectors.js
+++
@@ -1,4 +0,0 @@
-export const getIsLoggedIn = state => state.auth.isLoggedIn;
-export const getUserName = state => state.auth.user.name;
-export const getIsFetchingCurrentUser = state =>
-  state.auth.isFetchingCurrentUser;
