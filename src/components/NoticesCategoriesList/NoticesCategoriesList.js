c0c5d09c3a988ca474aaeb2d2800ed1ea68e4c38
Volkova Valeria <vvolk.valeria@gmail.com>
2023-03-20 20:39:22 +0200

    getNoticesByCategories

--- src/components/NoticesCategoriesList/NoticesCategoriesList.js
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.js
@@ -1,59 +1,84 @@
-//import {  useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { NoticeCategoryItem } from '../NoticeCategoryItem/NoticeCategoryItem';
-//import { getNotices } from "../../API/fetchNotice";
 import { AddNoticeButton } from "../AddNoticeButton/AddNoticeButton";
 import css from "./NoticesCategoriesList.module.css";
+import { useLocation } from 'react-router-dom';
+import { getNoticesByCategories } from "../../utilities/helpers";
+import { toast } from 'react-toastify';
 
-
-const items = [
-  {
-    id: 1,
-    category: 'sell',
-    title: 'dog',
-    breed: 'kolly',
-    location: 'Lviv',
-    age: '5',
-    price: 75,
-  },
-  {
-    id: 2,
-    category: 'lost-found',
-    title: 'dog',
-    breed: 'main-kun',
-    location: 'Kiev',
-    age: '2',
-  },
-  {
-    id: 3,
-    category: 'sell',
-    title: 'dog',
-    breed: 'corgi',
-    location: 'Kharkiv',
-    age: '1',
-    price: 50,
-  },
-];
+// const items = [
+//   {
+//     id: 1,
+//     category: 'sell',
+//     title: 'dog',
+//     breed: 'kolly',
+//     location: 'Lviv',
+//     age: '5',
+//     price: 75,
+//   },
+//   {
+//     id: 2,
+//     category: 'lost-found',
+//     title: 'dog',
+//     breed: 'main-kun',
+//     location: 'Kiev',
+//     age: '2',
+//   },
+//   {
+//     id: 3,
+//     category: 'sell',
+//     title: 'dog',
+//     breed: 'corgi',
+//     location: 'Kharkiv',
+//     age: '1',
+//     price: 50,
+//   },
+// ];
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
 
 const NoticesCategoriesList = ({ item }) => {
-  // const location = useLocation();
+
+  let { pathname } = useLocation();
+  const [notices, setNotices] = useState([]);
+  const [status, setStatus] = useState(Status.IDLE);
+
+let category = pathname.split("/").pop();
+
+  useEffect(() => {
+    if (!category && status === Status.IDLE) {
+      return;
+    }
+    getNoticesByCategories(category)
+    .then(data => {
+      setNotices(data);
+      setStatus(Status.RESOLVED)
+    })
+    .catch(error => {
+      setStatus(Status.REJECTED)
+      console.log('Error',error);
+    })
+  }, [category]);
+
+ // console.log("notices",notices); 
+
+  if (notices.length === 0 && status === Status.RESOLVED) {
+    toast.error('Nothing found for your request!');
+    return;
+  }
 
 
-//const category = "sell";
-
-
-
-  // useEffect(() => {
-  //     getNotices(category);
-  // }, [category]);
-
-    
     return (
         <>
       <AddNoticeButton/> 
 <ul className={css.list}>
 
-        {items.map(item => {
-          return <NoticeCategoryItem key={item.id} item={item} />;
+        {notices.map(notice => {
+          return <NoticeCategoryItem key={notice._id} item={notice} />;
         })}
       </ul>
     </>
