4ae981f555884ebf38973c68a0e9f8509414b0bd
Volkova Valeria <vvolk.valeria@gmail.com>
2023-03-20 02:43:17 +0200

    modal

---
+++ src/components/ModalAddNotice/ModalAddNoticeSecond.js
@@ -0,0 +1,20 @@
+///import { Formik, ErrorMessage, Form, Field } from 'formik';
+//import css from "./ModalAddNotice.module.css";
+
+export const ModalAddNoticeSecond = ({toggleModalPage, onSubmit}) => {
+
+    return (
+        
+        <>
+
+
+
+
+
+<button type="submit" onSubmit={onSubmit} >Done</button>
+<button onClick={toggleModalPage} type="submit">Back</button>
+        </>
+        
+            )
+
+}
