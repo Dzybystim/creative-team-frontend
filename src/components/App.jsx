fa2331867656e85df01fcd2d5dedd53417616c26
Katya <katyagilevaukr@gmail.com>
2023-03-20 17:49:40 +0200

    added user page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,67 @@
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
-import  Layout  from "components/Layout/Layout"
+import Layout from 'components/Layout/Layout';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
+import UserPage from 'pages/UserPage';
 
 const NoticesPage = lazy(() => import('../pages/NoticesPage'));
 const OurFriendsPage = lazy(() => import('../pages/OurFriendsPage'));
-const NoticesCategoriesList = lazy(() => import('../components/NoticesCategoriesList/NoticesCategoriesList'));
-const NotFound = lazy(() => import('../utilities/NotFound/NotFound'))
+const NoticesCategoriesList = lazy(() =>
+  import('../components/NoticesCategoriesList/NoticesCategoriesList')
+);
+
+const NotFound = lazy(() => import('../utilities/NotFound/NotFound'));
 
 export const App = () => {
-  return (<>
-  <Layout>
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<div>Header</div>} />
-        <Route path="news" element={<div>NewsPage</div>} />
-        <Route path="notices" element={<NoticesPage />}>
-          <Route path="sell" element={<NoticesCategoriesList />} />
-          <Route path="lost-found" element={<NoticesCategoriesList />} />
-          <Route path="for-free" element={<NoticesCategoriesList />} />
-          <Route path="favorite" element={<NoticesCategoriesList />} />
-          <Route path="own" element={<NoticesCategoriesList />} />
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<div>Header</div>} />
+            <Route path="user" element={<UserPage />} />
+            <Route path="news" element={<div>NewsPage</div>} />
+            <Route path="notices" element={<NoticesPage />}>
+              <Route path="sell" element={<NoticesCategoriesList />} />
+              <Route path="lost-found" element={<NoticesCategoriesList />} />
+              <Route path="for-free" element={<NoticesCategoriesList />} />
+              <Route path="favorite" element={<NoticesCategoriesList />} />
+              <Route path="own" element={<NoticesCategoriesList />} />
 
-          <Route path="title" element={<div>Title</div>} />
-          <Route path="category" element={<div>Category</div>} />
-          <Route path="selected" element={<div>Selected</div>} />
-          <Route
-            path="selected/:selectedId"
-            element={<div>Selected/selectedId</div>}
-          />
-          <Route path="user" element={<div>User</div>} />
-          <Route path="user/:noticeId" element={<div>Notice/noticeId</div>} />
-        </Route>
-        <Route path="notices/:noticeId" element={<div>Notices/noticeId</div>} />
-        <Route path="pets" element={<div>Pets</div>} />
-        <Route path="pets/:petId" element={<div>Pets/petId</div>} />
-        <Route path="friends" element={<OurFriendsPage />}></Route>
-        <Route path="userAndPets" element={<div>userAndPets</div>}></Route>
-        <Route path="users" element={<div>Users</div>}>
-          <Route path="signup" element={<div>Signup</div>} />
-          <Route path="login" element={<div>Login</div>} />
-          <Route path="edit" element={<div>Edit</div>} />
-          <Route path="logout" element={<div>Logout</div>} />
-        </Route>
-        <Route path="*" element={<NotFound/>} />
-      </Route>
-    </Routes>
-    <ToastContainer />
-    </Layout>
+              <Route path="title" element={<div>Title</div>} />
+              <Route path="category" element={<div>Category</div>} />
+              <Route path="selected" element={<div>Selected</div>} />
+              <Route
+                path="selected/:selectedId"
+                element={<div>Selected/selectedId</div>}
+              />
+              <Route path="user" element={<UserPage />} />
+              <Route
+                path="user/:noticeId"
+                element={<div>Notice/noticeId</div>}
+              />
+            </Route>
+            <Route
+              path="notices/:noticeId"
+              element={<div>Notices/noticeId</div>}
+            />
+            <Route path="pets" element={<div>Pets</div>} />
+            <Route path="pets/:petId" element={<div>Pets/petId</div>} />
+            <Route path="friends" element={<OurFriendsPage />}></Route>
+            <Route path="userAndPets" element={<div>userAndPets</div>}></Route>
+            <Route path="users" element={<div>Users</div>}>
+              <Route path="signup" element={<div>Signup</div>} />
+              <Route path="login" element={<div>Login</div>} />
+              <Route path="edit" element={<div>Edit</div>} />
+              <Route path="logout" element={<div>Logout</div>} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+        <ToastContainer />
+      </Layout>
     </>
   );
 };
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,18 +1,19 @@
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet, NavLink } from 'react-router-dom';
 
-
-
-export default function SharedLayout () {
-
-    return (<>
-        <nav>
-          <NavLink to="/" end>Header</NavLink>
-          <NavLink to="/news">News</NavLink>
-          <NavLink to="/notices">Find pet</NavLink>
-          <NavLink to="/friends">Our Friends</NavLink>
-          <NavLink to="/users">Users</NavLink>
-        </nav> 
-        <Outlet />
-      </>
-    )
+export default function SharedLayout() {
+  return (
+    <>
+      <nav>
+        <NavLink to="/" end>
+          Header
+        </NavLink>
+        <NavLink to="/news">News</NavLink>
+        <NavLink to="/notices">Find pet</NavLink>
+        <NavLink to="/friends">Our Friends</NavLink>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="/user">User</NavLink>
+      </nav>
+      <Outlet />
+    </>
+  );
 }
