56c1a2eb853da010e2e8399d842f321360f0c8fc
Katya <katyagilevaukr@gmail.com>
2023-03-22 13:12:01 +0200

    added notiflix, update userdata

--- src/components/UserPage/UserData/UserData.js
+++ src/components/UserPage/UserData/UserData.js
@@ -1,53 +1,65 @@
-//import PropTypes from 'prop-types';
 import css from './UserData.module.css';
+import axios from 'axios';
+//const API_KEY = '?api_key=0fe38363eb34760457702cbb346d2d7a';
 
-export default function UserData(data) {
+import { useParams } from 'react-router-dom';
+//import { getUserDataById } from 'service/API';
+//import { Suspense } from "react";
+import { useState, useEffect } from 'react';
+//import { Outlet} from "react-router-dom";
+import Notiflix from 'notiflix';
+const BASE_URL = 'https://backend.petly.club';
+
+//import NoImage from 'images/noImag.png';
+
+export const getUserDataById = noticeId =>
+  axios.get(`${BASE_URL}userData/${noticeId}`);
+
+export default function UserData() {
+  const { id } = useParams();
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    getUserDataById(id)
+      .then(({ data }) => setUserData(data))
+      .catch(error =>
+        Notiflix.Notify.warning(
+          'Sorry, something went wrong.... Please try again.'
+        )
+      );
+  }, [id]);
+
+  const { name, email, birthday, phone, city } = userData;
+
   return (
     <>
       <div>
-        <div className={css.avatar}>
-          <input
-            className={css.input}
-            type="file"
-            id="file"
-            accept=".jpg, .jpeg, .png"
-            hidden
-          ></input>
-          <button /*onclick="file.click()"*/>Edit photo</button>
+        <img src="" alt={name} width={'300px'} />
+        <div class={css.userinfo}>
+          <ul className={css.info}>
+            <li className={css.info_data}>
+              <p className={css.text}>Name:</p>
+              <p className={css.text}> {name}</p>
+            </li>
+            <li className={css.info_data}>
+              <p className={css.text}>Email:</p>
+              <p className={css.text}>{email}</p>
+            </li>
+            <li className={css.info_data}>
+              <p className={css.text}>Birthday:</p>
+              <p className={css.text}>{birthday}</p>
+            </li>
+            <li className={css.info_data}>
+              <p className={css.text}>Phone:</p>
+              <p className={css.text}>{phone}</p>
+            </li>
+            <li className={css.info_data}>
+              <p className={css.text}>City:</p>
+              <p className={css.text}>{city}</p>
+            </li>
+          </ul>
         </div>
-
-        <ul className={css.info}>
-          <li className={css.info_data}>
-            <p className={css.text}>Name:</p>
-            <p className={css.text}> {data.name}</p>
-          </li>
-          <li className={css.info_data}>
-            <p className={css.text}>Email:</p>
-            <p className={css.text}>{data.email}</p>
-          </li>
-          <li className={css.info_data}>
-            <p className={css.text}>Birthday:</p>
-            <p className={css.text}>{data.birthday}</p>
-          </li>
-          <li className={css.info_data}>
-            <p className={css.text}>Phone:</p>
-            <p className={css.text}>{data.phone}</p>
-          </li>
-          <li className={css.info_data}>
-            <p className={css.text}>City:</p>
-            <p className={css.text}>{data.city}</p>
-          </li>
-        </ul>
       </div>
     </>
   );
 }
-
-//UserData.propTypes = {
-//     id: PropTypes.string.isRequired,
-//     name: PropTypes.string.isRequired,
-//    email: PropTypes.string.isRequired,
-//     phone: PropTypes.number.isRequired,
-//     birthday: PropTypes.number.isRequired,
-//     city: PropTypes.string.isRequired,
-//};
