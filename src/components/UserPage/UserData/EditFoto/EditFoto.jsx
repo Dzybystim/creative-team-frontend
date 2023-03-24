2f877438e1feb27a3aabed4c9a50931333bf8d01
Anna Kovalchuk <annazukh@gmail.com>
2023-03-24 03:34:51 +0200

    upload file

--- src/components/UserPage/UserData/EditFoto/EditFoto.jsx
+++ src/components/UserPage/UserData/EditFoto/EditFoto.jsx
@@ -1,15 +1,36 @@
 import { HiOutlineCamera } from 'react-icons/hi2';
 import { postImageToStorage } from '../../../../utilities/helpers';
-import { useState } from 'react';
 import css from './EditFoto.module.css';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export default function EditFoto() {
-  const [selectedFile, setSelectedFile] = useState('');
+  const setAuthHeader = token => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  };
+
+  const clearAuthHeader = () => {
+    axios.defaults.headers.common.Authorization = '';
+  };
+
+  const tokenFromLocalStorage = localStorage.getItem('persist:users');
+
+  if (tokenFromLocalStorage !== null) {
+    const tokenParse = JSON.parse(tokenFromLocalStorage).token;
+    const token = tokenParse.slice(1, tokenParse.length - 1);
+    setAuthHeader(token);
+  } else {
+    clearAuthHeader();
+  }
+
   const handleUpload = async e => {
-    setSelectedFile(e.target.files[0]);
+    const file = e.target.files[0];
+    if (file.size > 375000) {
+      toast.error('Maximum file size is 3 MB');
+    }
 
     const formData = new FormData();
-    formData.append('avatar', selectedFile);
+    formData.append('avatar', file);
 
     postImageToStorage(formData)
       .then(data => {
