86e1d61f4f6a94fb5b6cee1f132420e5daddad71
JaRcomGitHub <jarcom@ukr.net>
2023-03-26 13:35:29 +0300

    news
    перенес логику в другой файл,
    добавил троеточия,
    преобразование даны в правильный формат,

--- src/components/News/NewsFormSearch.jsx
+++ src/components/News/NewsFormSearch.jsx
@@ -6,40 +6,39 @@
 // import { useState } from "react";
 // import { useTranslation } from 'react-i18next';
 
-// const FormSearchNews = () => {
-//    const [valueIn, setValueIn] = useState('');
+const FormSearchNews = () => {
+  //    const [valueIn, setValueIn] = useState('');
+  //    const { t } = useTranslation();
+  //    const dispatch = useDispatch();
+  // const onClickNews = (e) => {
+  //   e.preventDefault();
+  //   setValueIn('')
+  // };
 
-//    const { t } = useTranslation();
-//    const dispatch = useDispatch();
+  return (
+    <>
+      FormSearchNews
+      {/* <div className={css.container}>
+          <h2 className={css.title}>{t('news')}</h2>
+          <div className={css.position}>
+            <form onSubmit={onClickNews}>
+              <Input
+                customStyle={css.InputNews}
+                placeholder={'Search'}
+                //   value
+                label={
+                  <button className={css.buttonSearch} type="submit">
+                    <svg>
+                      <use />
+                    </svg>
+                  </button>
+                }
+              />
+            </form>
+          </div>
+        </div> */}
+    </>
+  );
+};
 
-// const onClickNews = (e) => {
-//   e.preventDefault();
-//   setValueIn('')
-
-// }
-
-//   return (
-//     <div className={css.container}>
-//       <h2 className={css.title}>{t('news')}</h2>
-//       <div className={css.position}>
-//         <form onSubmit={onClickNews}>
-//           <Input
-//           customStyle={css.InputNews}
-//           placeholder={'Search'}
-//         //   value
-//           label={
-//             <button className={css.buttonSearch} type="submit"
-//             >
-//               <svg>
-//                 <use />
-//               </svg>
-//             </button>
-//         }/>
-//            </form>
-//       </div>
-
-//     </div>
-//   );
-// };
-
-// export default FormSearchNews;
+export default FormSearchNews;
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,13 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { Dna } from 'react-loader-spinner';
+import { fetchNews } from 'utilities/helpers';
 import css from './NewsPage.module.css';
 import NewsList from 'components/News/NewsList';
-// import FormSearchNews from 'components/News/NewsFormSearch';
+import FormSearchNews from 'components/News/NewsFormSearch';
 
 function NewsPage() {
+  const [loading, setLoading] = useState(false);
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchNews()
+      .then(setNews)
+      .catch(error => console.error(error.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const getVisibleNews = () => {
+    return news.filter(news => news.title.toLocaleLowerCase());
+  };
+
   return (
     <div className={css.container}>
       <h2 className={css.title}>News</h2>
-      {/* <FormSearchNews /> */}
-      <NewsList />
+      <FormSearchNews />
+      {loading ? (
+        <div className={css.loading}>
+          <Dna
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
+        </div>
+      ) : (
+        <NewsList dataNewsList={getVisibleNews()} />
+      )}
     </div>
   );
 }
