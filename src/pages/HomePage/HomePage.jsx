b6ed15e3dbed153f375342e0b0a3ced1c227da39
Olha Chornei <pradocw8053@gmail.com>
2023-03-31 15:22:23 +0300

    add

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,3 @@
-// import dogMobile from '../../images/mobile/dogMobile.png';
-// import dogTablet from '../../images/tablet/dogTablet.png';
-// import dogDesktop from '../../images/desktop/dogDesktop.png';
-// import unionDesk from '../../images/unionDesk.svg';
-// import waveDesktop from '../../images/desktop/waveDesktop.png';
-// import waveDesktop2 from '../../images/desktop/waveDesktop2.png';
-// import waveMobile from '../../images/mobile/waveMobile.png';
-// import waveTablet from '../../images/tablet/waveTablet.png';
-
 import css from '../HomePage/HomePage.module.css';
 
 function HomePage() {
